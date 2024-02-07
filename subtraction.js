0523e834eb3ea2749d5b8e56c08c04f33c8e9294
Sisekocom <nonyongos1@gmail.com>
2024-02-07 10:26:39 +0200

    subtraction

---
+++ subtraction.js
@@ -0,0 +1,12 @@
+function subtraction(a,b){
+    return a-b
+}
+
+module.exports = {
+    subtraction
+}
+
+exports.subtraction = (a,b)=> {
+    return b > a ?`$(b) is great then $(a)`:
+    q - b
+}
